1d969d9adfe7cbe4e52509e8071d7c7713113d43
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2024-08-20 14:38:56 -0300

    Implementamos tests

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Pizza::factory(10)->create();
+        \App\Models\Pizza::factory(1)->create([
+            "id" => 5000,
+            "deleted_at" => null
+        ]);
+        \App\Models\Pizza::factory(1)->create([
+            "id" => 5001,
+            "deleted_at" => null
+        ]);
     }
 }
